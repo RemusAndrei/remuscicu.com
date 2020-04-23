47e47d42f10790eb0b5c691e725c4022556e9c1a
Remus Cicu <remuscicu@gmail.com>
2020-04-23 13:50:00 +0200

    small fix

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@
 
     (() => {
         // Specify the deadline date
-        const deadlineDate = new Date('June 30, 2019 23:59:59').getTime();
+        const deadlineDate = new Date('June 30, 2020 23:59:59').getTime();
         
         // Cache all countdown boxes into consts
         const countdownDays = document.querySelector('.countdown__days .number');
